e17f6f648bceb047c0ec3b26d3ffc3fbfaf34582
Jherson <jherson.jose.os@gmail.com>
2020-11-24 09:31:06 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,56 +1,21 @@
-$(window).ready(function(){
-    $(window).scroll(function (event) {
-        var scroll = $(window).scrollTop();
-       if(scroll > 20){
-        $("header").addClass("active")
-       }else{
-        $("header").removeClass("active")   
-       }
-    });
-    
-    
-    $('.x').slick({
-        dots: false,
-        infinite: false,
-        autoplay:true,
-        autoplaySpeed: 2000,
-        arrows:false,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
+var img = document.querySelectorAll(".pics .img-fluid");
+var close_ = document.getElementById("close_");
+var close_wsp = document.getElementById("close_wsp");
+for (let index = 0; index < img.length; index++) {
+  
+  img[index].addEventListener("click",function(){
+    var attr = this.getAttribute("src");
+    document.getElementById("img").setAttribute("src",attr);
+    document.getElementById("modal_img").classList.add("active");
+  })
+  
+};
 
+close_.addEventListener("click",function(){
+  document.getElementById("modal_img").classList.remove("active");
+});
 
-      $(".menu__").click(function(){
-        $(".menu").toggleClass("active")
-      })
-
+close_wsp.addEventListener("click",function(e){
+  e.preventDefault();
+  document.querySelector(".msg").classList.add("none");
 })
